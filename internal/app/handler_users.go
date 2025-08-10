d5de7c1d3dedecf505f9f0f0e89de714836bd68e
rdcassin <rdcassin@gmail.com>
2025-08-09 23:13:09 -0500

    comment for next plans

--- internal/app/handler_users.go
+++ internal/app/handler_users.go
@@ -10,7 +10,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// Need to add the other relations.
+// Need to add the other relati
 type CreateUserInput struct {
 	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
